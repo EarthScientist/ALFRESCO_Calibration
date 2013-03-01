4b1ce331a5212c7f03c61eae7686e283c954f968
Michael Lindgren <lindgren.mike@gmail.com>
2013-03-01 06:56:57 -0900

    a version of the script for Anna that will grab fire size events.  First commit.

--- ALFRESCO_rep_calibration.r
+++ ALFRESCO_rep_calibration.r
@@ -1,10 +1,11 @@
 # user inputs to script:
-workingDir = "/workspace/UA/malindgren/projects/ALFRESCO/Calibration_Script/"
-mapsPath = "/workspace/Shared/ALFRESCO_Jacob_Bird/NEW_CaribouSubregionResults/CalibrationResults_Maps1950to2009/Maps/"
-mcfPath = "/workspace/UA/malindgren/projects/ALFRESCO/Calibration_Script/code_copy/multicore_function_parallel.r"
-nreps = 0:1
-years = 1950:1951
-maskFile = "/workspace/UA/malindgren/projects/ALFRESCO/Calibration_Script/masks/Boreal_Tundra_CombinedDomains.shp"
+workingDir = "./"
+mapsPath = "./Maps/"
+mcfPath = "/home/UA/apbennett/ALFRESCO_Calibration/multicore_function_parallel.r"
+nreps = 0:95
+years = 1950:2011
+maskFile = "/home/UA/apbennett/ALFRESCO_Calibration/Boreal_Tundra_CombinedDomains.shp"
+
 
 #  SHOULD ONLY HAVE TO TOUCH THE VARIABLES ABOVE TO GET IT RUNNING ###
 
@@ -35,7 +36,7 @@ if(extension(maskFile) == ".shp"){
 rm(template,shp)
 
 # source in matts parallel setup function
-source(mcfPath)
+source(mcfPath) #"/workspace/UA/malindgren/projects/ALFRESCO/Calibration_Script/code_copy/multicore_function_parallel.r"
 
 # how many cores to use?
 cpu.count <- detectCores()
@@ -43,43 +44,46 @@ cpu.count <- detectCores()
 # func
 f <- function(nreps){
 	bor.tab <- numeric()
-	bor.size <- numeric()
+	bor.size.mean <- numeric()
 	bor.num <- numeric()
+    bor.size <- numeric()
 	tun.tab <- numeric()
-	tun.size <- numeric()
+	tun.size.mean <- numeric()
 	tun.num <- numeric()
-	boreal.tundra.v <- getValues(boreal.tundra)
+    tun.size <- numeric()
+	
 	for(nrep in nreps){
 		for(year in years){
-			r <- raster(paste(dataPath,"Age_",nrep,"_",year,".tif",sep=""))
+			r <- raster(paste(dataPath,"Age_",nrep,"_",year+1,".tif",sep=""))
 			r[which(values(r) != 0)] <- NA
 			r[which(values(r) == 0)] <- 1
-			#create contiguous patches
-			r.clump <- clump(r,gaps=FALSE) #[which(values(boreal.tundra)==1),drop=F]
-			r.clump.v <- getValues(r.clump)
 			#boreal
-			boreal.patch <- as.numeric(table(r.clump.v[which(boreal.tundra.v == 1)], useNA='no'))
-			bor.tab <- append(bor.tab, sum(boreal.patch))
-			bor.size <- append(bor.size, mean(boreal.patch))
-			bor.num <- append(bor.num, length(boreal.patch))
+			r.clump <- clump(r,gaps=FALSE) #[which(values(boreal.tundra)==1),drop=F]
+			boreal.patch <- freq(r.clump[values(boreal.tundra == 1), drop=F], useNA='no')
+			bor.tab <- append(bor.tab, sum(boreal.patch[,2]))
+			bor.size.mean <- append(bor.size, mean(boreal.patch[,2]))
+			bor.size <- append(bor.size, boreal.patch[,2])
+			bor.num <- append(bor.num, nrow(boreal.patch))
 			#tundra
-			tundra.patch <- table(r.clump.v[which(boreal.tundra.v == 2)], useNA='no')
-			tun.tab <- append(tun.tab, sum(tundra.patch))
-			tun.size <- append(tun.size, mean(tundra.patch))
-			tun.num <- append(tun.num, length(tundra.patch))
+			tundra.patch <- freq(r.clump[values(boreal.tundra == 2), drop=F], useNA='no')
+			tun.tab <- append(tun.tab, sum(tundra.patch[,2]))
+			tun.size.mean <- append(tun.size, mean(tundra.patch[,2]))
+			tun.size <- append(tun.size, tundra.patch[,2])
+			tun.num <- append(tun.num, nrow(tundra.patch))
 		}
 	}
-	out <- list(bor.tab, bor.size, bor.num, tun.tab, tun.size, tun.num)
+	out <- list(bor.tab, bor.size.mean, bor.num, tun.tab, tun.size.mean, tun.num, bor.size, tun.size)
 }
 
 # run matt's multicore function with the function laid out above
 t3 <- mcf(f, iters=nreps, clusters=cpu.count)
 
 # names of the needed output files
-table_names = c("boreal.aab","boreal.size.fire","boreal.num.fire","tundra.aab","tundra.size.fire","tundra.num.fire")
-#table_names = c(boreal.aab,boreal.size.fire,boreal.num.fire,tundra.aab,tundra.size.fire,tundra.num.fire)
+table_names = c("boreal.aab","boreal.size.fire.mean","boreal.num.fire","tundra.aab","tundra.size.fire.mean","tundra.num.fire","boreal.size.fire","tundra.size.fire")
+# table_names = c(boreal.aab,boreal.size.fire,boreal.num.fire,tundra.aab,tundra.size.fire,tundra.num.fire)
 #create the needed output tables from the nested list object created above
-for(i in 1:length(table_names)){
+#for(i in 1:(length(table_names)){
+for(i in 1:6){
 	assign(as.character(table_names[i]), matrix(unlist(lapply(t3,'[[',i)), nrow=length(years)))
 	if(any(is.na(table_names[i])) == TRUE){
 		print("TRUE!")
@@ -87,7 +91,17 @@ for(i in 1:length(table_names)){
 	}
 }
 
+for(i in 7:8){
+	assign(as.character(table_names[i]), unlist(lapply(t3,'[[',i))) 
+	if(any(is.na(table_names[i])) == TRUE){
+		print("TRUE!")
+		table_names[i][which(is.na(table_names[i]) == TRUE)] <- 0
+	}
+}
+
+
 #lets remove some of the objects in memory
+reps=nreps
 rm(table_names, i, nreps, years)
 
 #save the workspace image
